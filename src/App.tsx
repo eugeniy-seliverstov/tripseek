6bf1ecdabcea0cb0d76b0d853851352b76ac4aea
Eugeniy Seliverstov <serdsaerrr@mail.ru>
2024-09-07 18:07:52 +0500

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="w-full h-screen flex flex-col sm:flex-row overflow-hidden">
+      <div
+        className="w-full sm:w-auto h-full grow"
+        style={{ background: 'linear-gradient(180deg,#1a0548 0,#1a0548 80%,#1e055e 88%,#300a83' }}
+      >
+        {/* Map */}
+      </div>
+      <div className="w-full sm:w-2/5 md:1/3 lg:w-2/5 xl:w-1/4 max-w-md h-full bg-white">
+        {/* Sidebar */}
+      </div>
+    </div>
   )
 }
 
