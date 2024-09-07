df1497a745a0f25c3569ffb7573d29b49ff02265
Eugeniy Seliverstov <serdsaerrr@mail.ru>
2024-09-07 17:02:43 +0500

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-
 function App() {
   return (
     <>
-      <h1>Skratch Friends</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
