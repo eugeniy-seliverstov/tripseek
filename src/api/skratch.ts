84ca4a890c7e4c4c31ede56f7daaaa3ea2d57cec
Eugeniy Seliverstov <serdsaerrr@mail.ru>
2025-03-13 02:49:42 +0500

    Fix skrach api request

--- src/api/skratch.ts
+++ src/api/skratch.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 
-const VITE_SKRATCH_API = 'https://api2.skratch.world/'
+const VITE_SKRATCH_API = 'https://api2.skratch.world'
 
-export const getUserData = (secret: string): Promise<AxiosResponse> => {
-  return axios.get(`${VITE_SKRATCH_API}/v2/users/secret/${secret}`)
+export const getUserData = (userId: string): Promise<AxiosResponse> => {
+  return axios.get(`${VITE_SKRATCH_API}/share-api/user/${userId}`)
 }
