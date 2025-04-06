7596f774f796aba7992799cfe1660bd7edc5b153
Eugeniy Seliverstov <serdsaerrr@mail.ru>
2025-04-06 21:13:37 +0500

    Change logo

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,11 +1,11 @@
 
-import logoCirlce from '@/assets/logo/skratch-logo-circle.svg'
-import logoAlpha from '@/assets/logo/skratch-logo-alpha.svg'
+import logoCirlce from '@/assets/logo.svg'
+import logoAlpha from '@/assets/name.svg'
 
 function Logo() {
   return (
     <div className="inline-flex items-center gap-2.5 ml-6">
-      <img src={logoCirlce} alt="Logo" />
+      <img src={logoCirlce} alt="Logo" width="32"/>
       <img src={logoAlpha} alt="Logo Alpha" />
     </div>
   )
