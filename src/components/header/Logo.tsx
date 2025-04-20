b7aebe69668f8007d3a1019fbfe8289724668b3e
Eugeniy Seliverstov <serdsaerrr@gmail.com>
2025-04-20 19:42:34 +0500

    Change the logo

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,12 +1,11 @@
 
-import logoCirlce from '@/assets/logo.svg'
-import logoAlpha from '@/assets/name.svg'
+import logo from '@/assets/logo.png'
 
 function Logo() {
   return (
     <div className="inline-flex items-center gap-2.5 ml-6">
-      <img src={logoCirlce} alt="Logo" width="32"/>
-      <img src={logoAlpha} alt="Logo Alpha" />
+      <img src={logo} alt="Logo" width="32"/>
+      <span style={{ fontFamily: 'Philosopher' }} className='text-3xl text-white'>Reworld</span>
     </div>
   )
 }
