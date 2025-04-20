266f550c67e2d8d570599d24aa1f05ecf8979c1b
Eugeniy Seliverstov <serdsaerrr@gmail.com>
2025-04-20 19:48:02 +0500

    Remove unused code

--- src/api/skratch.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosResponse } from 'axios'
-
-const VITE_SKRATCH_API = 'https://api2.skratch.world'
-
-export const getUserData = (userId: string): Promise<AxiosResponse> => {
-  return axios.get(`${VITE_SKRATCH_API}/share-api/user/${userId}`)
-}
