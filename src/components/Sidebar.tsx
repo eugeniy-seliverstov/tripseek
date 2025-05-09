9753a4ffac2228385773f91db985449d32e45252
Eugeniy Seliverstov <serdsaerrr@gmail.com>
2025-05-09 23:20:22 +0500

    Set All filter if visited is empty

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,17 @@
-import { JSX } from 'react'
+import { useEffect, JSX } from 'react'
 import Header from '@/components/sidebar/Header'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import AllTerritories from '@/components/sidebar/filters/AllTerritories'
 import VisitedTerritories from '@/components/sidebar/filters/VisitedTerritories'
 import WishlistTerritories from '@/components/sidebar/filters/WishlistTerritories'
 
+import useUserStore from '@/store/useUserStore'
 import useFilterStore from '@/store/useFilterStore'
 import type { Filter } from '@/store/useFilterStore'
 
 function Sidebar() {
-  const { filter, setFilter } = useFilterStore()
+  const { filter, setFilter, initFilter } = useFilterStore()
+  const { visited } = useUserStore()
 
   const filterViews: Record<Filter, JSX.Element> = {
     visited: <VisitedTerritories />,
@@ -17,11 +19,15 @@ function Sidebar() {
     all: <AllTerritories />
   }
 
+  useEffect(() => {
+    initFilter(visited.length)
+  }, [])
+
   return (
     <div className="w-full sm:w-2/5 max-w-md h-full bg-white overflow-auto">
       <Header />
 
-      <Tabs defaultValue={filter} onValueChange={(value) => setFilter(value as Filter)} className="w-full px-4">
+      <Tabs value={filter} onValueChange={(value) => setFilter(value as Filter)} className="w-full px-4">
         <TabsList className="w-full flex justify-between">
           <TabsTrigger value="visited" className="flex-1">Visited</TabsTrigger>
           <TabsTrigger value="wishlist" className="flex-1">Want to visit</TabsTrigger>
--- src/store/useFilterStore.ts
+++ src/store/useFilterStore.ts
@@ -5,11 +5,13 @@ export type Filter = 'visited' | 'wishlist' | 'all'
 export interface FilterStore {
   filter: Filter
   setFilter: (filter: Filter) => void
+  initFilter: (count: number) => void
 }
 
 const useFilterStore = create<FilterStore>((set) => ({
   filter: 'visited',
   setFilter: (filter) => set(() => ({ filter })),
+  initFilter: (count) => set(() => ({ filter: count ? 'visited' : 'all' })),
 }))
 
 export default useFilterStore
