bdb41c2492a6d221e8018c39799c96e5f489e981
Eugeniy Seliverstov <serdsaerrr@gmail.com>
2025-05-18 18:22:17 +0500

    feat: Add sceleton loading for flags

--- src/components/sidebar/territory/TerritoryFlag.tsx
+++ src/components/sidebar/territory/TerritoryFlag.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { useInView } from 'react-intersection-observer'
+
 import type { ReactElement } from 'react'
 
 import { TerritoryCode } from '@/types/territory'
@@ -7,5 +10,26 @@ interface TerritoryFlagProps {
 }
 
 export const TerritoryFlag = ({ code }: TerritoryFlagProps): ReactElement => {
-  return <img className='w-10 h-10 select-none' src={`/flags/${code}.png`} />
+  const [isLoaded, setIsLoaded] = useState(false)
+  const { ref, inView } = useInView({ triggerOnce: true, rootMargin: '100px 0px' })
+
+  return (
+    <div
+      ref={ref}
+      className='relative w-10 h-10 rounded overflow-hidden flex items-center justify-center'
+    >
+      {inView || isLoaded ? (
+        <img
+          src={`/flags/${code}.png`}
+          alt={`${code} flag`}
+          className={`absolute w-full h-full object-contain`}
+          onLoad={() => setIsLoaded(true)}
+          loading='lazy'
+          decoding='async'
+        />
+      ) : null}
+
+      {!isLoaded && <div className='absolute w-10 h-7 rounded bg-gray-300 animate-pulse z-0' />}
+    </div>
+  )
 }
