8acef2f991828c4c6cdd11263647586d30f4b1fe
Eugeniy Seliverstov <serdsaerrr@gmail.com>
2025-06-08 23:16:55 +0500

    fix: Remove left breakpoint

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          'flex h-9 w-full rounded-md border border-border bg-transparent px-3 py-1 text-base shadow-sm transition-color placeholder:text-text-muted focus-visible:outline-none focus-visible:border-focus-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+          'flex h-9 w-full rounded-md border border-border bg-transparent px-3 py-1 text-base shadow-sm transition-color placeholder:text-text-muted focus-visible:outline-none focus-visible:border-focus-ring disabled:cursor-not-allowed disabled:opacity-50',
           className,
         )}
         ref={ref}
